27228051dc9a1ac9150115a2594380816b2dd4e0
PauloRobertoAlexandreSilva <pbetoas@gmail.com>
2021-07-14 17:32:22 -0300

    nova

--- js/qrcodeCreate.js
+++ js/qrcodeCreate.js
@@ -1,10 +1,14 @@
-window.addEventListener('load', function() {
+function CriaQRCode(tag, texto) {
     var options = {
         width: 180,
         height: 180,
         background:"#ffffff",
         foreground:"#000000",
-        text: 'https://paulorobertoalexandresilva.github.io/servical/'
+        text: texto
     }
-    $("#qrcode").qrcode(options);
+    tag.qrcode(options);
+}
+
+window.addEventListener('load', function() {
+    CriaQRCode($("#qrcode"), 'https://paulorobertoalexandresilva.github.io/servical/');
 });
